64bdeeeb8338112ee3ce2301b58b8097a8d960a7
lv1codeman <lv1codeman@gmail.com>
2022-01-24 17:01:02 +0800

    建立範本頁面

---
+++ js/setTitle.js
@@ -0,0 +1,12 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    //暫不使用，會亂碼
+
+    //以檔名設定html的<title>
+    //document.title = (location.pathname.substring(location.pathname.lastIndexOf("/") + 1)).replace('.html', '');
+
+    //設定頁面標題
+    //$("#pageTitle").text(document.title);
+
+})(jQuery); // End of use strict
